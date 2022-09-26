ace27a682c45338e7fceebd104a739f6b325a335
Rob Fox <60361504+foxxxxxxx7@users.noreply.github.com>
2022-09-26 12:46:16 +0100

    Week 3 Lab A Done

--- tasky/src/App.js
+++ tasky/src/App.js
@@ -1,28 +1,85 @@
 import './App.css';
 import Task from './components/Task';
 import React, { useState } from 'react';
+import AddTaskForm from './components/Form';
+import { v4 as uuidv4 } from 'uuid';
 
 function App() {
-  const [ taskState, setTaskState ] = useState({
+  const [taskState, setTaskState] = useState({
     tasks: [
-      { id: 1, title:"Dishes", description: "Empty dishwasher", deadline: "Today" },
-      {id: 2, title: "Laundry", description: "Fold clothes and put away", deadline: "Tomorrow" },
-      { id: 3, title: "Tidy up", deadline: "Today" },
-      {id: 4, title: "Get up out the bed", description: "Ye dirty beggar", deadline: "Yesterday" },
-      {id: 5, title: "Brush Teeth", deadline: "Now" }
+      { id: 1, title: "Dishes", description: "Empty dishwasher", deadline: "Today", done: false },
+      { id: 2, title: "Laundry", description: "Fold clothes and put away", deadline: "Tomorrow", done: false },
+      { id: 3, title: "Tidy up", deadline: "Today", done: false }
     ]
+
+  })
+  const [ formState, setFormState ] = useState({
+    title: "",
+    description: "",
+    deadline: ""
   });
+
+  const doneHandler = (taskIndex) => {
+    const tasks = [...taskState.tasks];
+    tasks[taskIndex].done = !tasks[taskIndex].done;
+    setTaskState({ tasks });
+
+    console.log(`${taskIndex} ${tasks[taskIndex].done}`);//. just to show the True/False result in the console log
+  }
+
+  const deleteHandler = (taskIndex) => {
+    const tasks = [...taskState.tasks];
+    tasks.splice(taskIndex, 1);
+    setTaskState({ tasks });
+  }
+
+  const formChangeHandler = (event) => {
+    let form = {...formState};
+
+    switch(event.target.name) {
+      case "title":
+          form.title = event.target.value;
+          break;
+      case "description":
+          form.description = event.target.value;
+          break;
+      case "deadline":
+          form.deadline = event.target.value;
+          break;
+      default:
+          form = formState;
+    }
+    setFormState(form);
+  }
+  console.log(formState);
+
+  const formSubmitHandler = (event) => {
+    event.preventDefault();
+
+    const tasks = [...taskState.tasks];
+    const form = {...formState};
+
+    form.id = uuidv4();
+
+    tasks.push(form);
+    setTaskState({tasks});
+  }
+
   return (
     <div className="container">
       <h1>Tasky</h1>
-      {taskState.tasks.map((task) => (              
-    <Task 
-      title={task.title}
-      description={task.description}
-      deadline={task.deadline}
-      key={task.id}
-    />
-  ))} 
+      {taskState.tasks.map((task, index) => (
+        <Task
+          title={task.title}
+          description={task.description}
+          deadline={task.deadline}
+          done={task.done}
+          key={task.id}
+          deleteTask={() => deleteHandler(index)}
+          markDone={() => doneHandler(index)}
+        />
+      ))}
+      <AddTaskForm submit={formSubmitHandler} change={formChangeHandler} />
     </div>
   );
 }
